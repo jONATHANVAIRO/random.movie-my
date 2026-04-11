e2ace2dac153dd69df6c91e8ac38016797d1da15
jONATHANVAIRO <116118808+jONATHANVAIRO@users.noreply.github.com>
2026-04-11 02:08:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,123 @@
+const peliculas = [
+
+{
+titulo:"Hombre Irracional",
+poster:"https://cdn.cinencuentro.com/wp-content/uploads/2016/01/irrational_man-italian-poster.jpg",
+link:"https://www.netflix.com/title/80039308"
+},
+
+{
+titulo:"Una batalla tras otra",
+poster:"https://pics.filmaffinity.com/one_battle_after_another-992235034-mmed.jpg",
+link:"https://play.hbomax.com/movie/bebe611d-8178-481a-a4f2-de743b5b135a"
+},
+{
+titulo:"PROJECT HAIL MARY",
+poster:"https://pics.filmaffinity.com/Proyecto_Fin_del_mundo-664968280-large.jpg",
+link:""
+},
+{
+titulo:"In the mood for love",
+poster:"https://m.media-amazon.com/images/I/61VdSeKt8SL._AC_UF894,1000_QL80_.jpg",
+link:"https://m.ok.ru/video/4198068652660"
+},
+{
+titulo:"DEMOLITION",
+poster:"https://m.media-amazon.com/images/M/MV5BNTEwYmRiZWMtOTM4OS00YzA0LTgyMWUtOTk5NzZkZDBmNWE0XkEyXkFqcGc@._V1_.jpg",
+link:"https://play.hbomax.com/movie/90533752-ae6d-47da-b0ff-423f72474760"
+},
+
+{
+titulo:"tHE WORST PERSON IN THE WORLD",
+poster:"https://m.media-amazon.com/images/I/711SfXTdjsL._AC_UF894,1000_QL80_.jpg",
+link:""
+},
+{
+titulo:"HAMNET",
+poster:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2SrMQ-MzYgp8_IVmZXb4hR2E_pT5e8w0HIW-j4vW6U9V11Fw36RrUalCtyHFqOSvi6l2wSkTTiZKDnNe8t1FtDYof0sbmEF5LooWCLw&s=10",
+link:""
+},
+{
+titulo:"ANNIE HALL",
+poster:"https://m.media-amazon.com/images/M/MV5BNjZlOWE5N2UtMTdhZC00NjU5LTg2MjYtOGU4NDhhNWNlMmRlXkEyXkFqcGc@._V1_.jpg",
+link:"https://m.ok.ru/video/91364330056"
+},
+
+{
+titulo:"Valor Sentimental",
+poster:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSB9-2IML2pHELLggTERQ9hyGYeNs18AnmdBUH6q23czATskVdA_w83P8NlSGvCDTk4WMCHe95YgBt7dQh-7mlt-fsQqikP1CR_46GgbrA&s=10"
+},
+
+{
+titulo:"El Drama",
+poster:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR48QvadpBoQfQ9h99q3W6U9ffbal835sDfUG50C2e50LA_CWGb9VsNZRsDElEbpCEVK22wBUKTjsbAkAaqk6eRgtkSzK5XgVGKkIdwWkc&s=10"
+}
+
+];
+
+
+const carousel = document.getElementById("carousel");
+
+peliculas.forEach(p => {
+
+carousel.innerHTML += `
+<div class="swiper-slide">
+<img src="${p.poster}">
+</div>
+`;
+
+});
+
+new Swiper(".mySwiper", {
+
+slidesPerView:4.3,
+spaceBetween:20,
+loop:true,
+
+grabCursor:true,
+
+navigation: {
+nextEl: ".swiper-button-next",
+prevEl: ".swiper-button-prev"
+},
+
+breakpoints: {
+
+0: {
+slidesPerView:2
+},
+
+600: {
+slidesPerView:3
+},
+
+900: {
+slidesPerView:4.3
+}
+
+}
+
+});
+
+function randomMovie(){
+
+const random = Math.floor(Math.random()*peliculas.length);
+
+const poster = document.getElementById("posterGrande");
+
+poster.classList.remove("animar");
+
+setTimeout(()=>{
+
+poster.src = peliculas[random].poster;
+document.getElementById("titulo").innerText =
+peliculas[random].titulo;
+
+document.getElementById("verPeli").href =
+peliculas[random].link;
+
+poster.classList.add("animar");
+
+},100);
+
+}
